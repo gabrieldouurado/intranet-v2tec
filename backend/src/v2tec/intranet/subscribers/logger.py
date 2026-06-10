650182ccfc2d83e909012d93f0f2d085de4b68a2
João Gabriel Dourado <joao.dourado96@gmail.com>
2026-06-10 13:38:12 -0300

    Exercicio 43

---
+++ backend/src/v2tec/intranet/subscribers/logger.py
@@ -0,0 +1,10 @@
+from v2tec.intranet import logger
+from zope.interface import Interface
+from zope.interface.interfaces import IObjectEvent
+
+DEBUG = False
+
+def debug_logs(obj: Interface, event: IObjectEvent):
+    """Log events for debugging purposes."""
+    if DEBUG:
+        logger.info(f"Event: {event}, Object: {obj}")
